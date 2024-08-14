5d410e05cfa13f73e8913f7c8bda70dd75d978ab
Kristian Bergmann <kristian.bergmann@codecentric.de>
2024-08-15 00:18:58 +0200

    add first sample

---
+++ go/code_sample_1/variant_a.go
@@ -0,0 +1,58 @@
+package birthday
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"net"
+	"net/smtp"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func SendGreetingsA(filename string, now time.Time, smtpHost string, smtpPort int) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	r.TrimLeadingSpace = true
+	header := false
+	for {
+		rec, err := r.Read()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if !header {
+			header = true
+			continue
+		}
+		// Lastname, Firstname, dateOfBirth, email
+		e, err := NewEmployee(rec[0], rec[1], rec[2], rec[3])
+		if err != nil {
+			return err
+		}
+		if e.IsBirthday(now) {
+			body := strings.Replace("Happy Birthday, dear %NAME%", "%NAME%", e.Firstname, -1)
+			err = sendMessageA(smtpHost, smtpPort, "sender@here.com", "Happy Birthday!", body, e.Email)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func sendMessageA(smtpHost string, smtpPort int, sender, subject, body, recipient string) error {
+	smtpAddr := net.JoinHostPort(smtpHost, strconv.Itoa(smtpPort))
+	msg := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n"+"%s\r\n", recipient, subject, body))
+	return smtp.SendMail(smtpAddr, nil, sender, []string{recipient}, msg)
+}
